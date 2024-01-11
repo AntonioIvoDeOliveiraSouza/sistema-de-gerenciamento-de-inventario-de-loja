8e987bd10b1dab46ec97122bba35e0089ef9a740
ANTONIO IVO DE OLIVEIRA SOUZA <2023113TADS0025@ifpi.int>
2024-01-11 16:45:26 -0300

    implementação de dois meios de estoque - interface

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@
         
         - Classe para produtos[FEITO], transações e relatórios
         - Subclasses para diferentes categorias de produtos [FEITO]
-        - Diferentes implementações de estoque
+        - Diferentes implementações de estoque [FEITO]
         - Tratamento de casos como falta de estoque ou erros de entrada de dados[50%]
 
     *É Necessário criar o atributo limite e elaborar meio de utilizá-lo
@@ -24,6 +24,55 @@
 
 //Criação de Array para gravar todos os registros de venda
 let listaCompleta: string[] = [];
+
+//Interface para Gerenciamento de estoque
+interface gerenciaEstoque{
+    checaEstoque():void ;
+    atualizaEstoque(quantidade:number):void ;
+}
+
+//Classe para estoque básico - implementação de interface gerenciaEstoque
+class estoqueBasico implements gerenciaEstoque{
+    private _quantidade:number;
+
+    constructor(quantidadeInicial:number){
+        this._quantidade = quantidadeInicial;
+    }
+
+    checaEstoque(){
+        console.log(`Quantidade em estoque atual:${this._quantidade} unidades`);
+    }
+
+    atualizaEstoque(quantidade:number){
+        this._quantidade +=quantidade;
+        console.log(`Estoque atualizado - Novo estoque:${this._quantidade} unidades`);
+    }
+}
+
+//Classe para Estoque limite - também implementação de interface gerenciaEstoque
+class estoqueLimite implements gerenciaEstoque{
+    private _quantidade:number;
+    private _limite:number;
+
+    constructor(quantidadeInicial:number, limite:number){
+        this._quantidade = quantidadeInicial;
+        this._limite = limite;
+    }
+
+    checaEstoque(){
+        console.log(`Quantidade em estoque atual:${this._quantidade} unidades`);
+    }
+
+    atualizaEstoque(quantidade: number): void {
+        this._quantidade +=quantidade;
+        console.log(`Estoque atualizado - Novo estoque:${this._quantidade} unidades`);
+
+        if(this._quantidade<=this._limite){
+            console.log(`ALERTA: QUANTIDADE ABAIXO DO LIMITE!\nEstoque: ${this._quantidade}| Mínimo necessário:${this._limite}`);
+        }
+
+    }
+}
 
 //Função para ler todos os registros da lista de vendas e compras
 function verLista(){
@@ -42,13 +91,15 @@ class Produto{
     protected _limite:number;
     protected _preco:number;
     protected _info:string =  "";
+    protected _estoque:gerenciaEstoque;
 
-    constructor(codigo:number, nome:string, quantidade:number, limite:number, preco:number){
+    constructor(codigo:number, nome:string, quantidade:number, limite:number, preco:number, estoque:gerenciaEstoque){
         this._codigo = codigo;
         this._nome = nome;
         this._quantidade = quantidade;
         this._limite = limite;
         this._preco = preco;
+        this._estoque = estoque;
 
         Produto.listaProdutos.push(this); //Após cada constructor (instancia), o objeto é registrado no array
     }
@@ -85,7 +136,7 @@ class Produto{
         if(venda>this._quantidade){
             console.error("Erro: Venda é superior ao estoque");
         }else{
-            this._quantidade-=venda;
+            this._estoque.atualizaEstoque(-venda);
             console.log(`Venda realizada com sucesso: Quantidade:${venda} ${this._nome}`);
             //Inserir informacao da venda em registro de produto
             this._info += `\nVenda: QUANTIDADE:${venda} | PRODUTO:${this._nome} | PREÇO/UNIDADE:${this._preco} R$| PREÇO/TOTAL:${venda * this._preco} R$`;
@@ -103,7 +154,7 @@ class Produto{
         if(compra<=0){
             console.error("Erro: Compra negativa e nula não existe");
         }else{
-            this._quantidade+=compra;
+            this._estoque.atualizaEstoque(compra);
             console.log(`Compra realizada com sucesso: Quantidade:${compra} ${this._nome}`);
             //Inserir informacao da compra em registro de produto
             this._info += `\nCompra: QUANTIDADE:${compra} | PRODUTO:${this._nome} | PREÇO/UNIDADE:${this._preco} R$| PREÇO/TOTAL:${compra * this._preco} R$`;
@@ -131,20 +182,21 @@ class Produto{
 
 //Implementação de subclasses
 class Informatica extends Produto{
-    constructor(codigo:number, nome:string, quantidade:number, limite:number, preco:number){
-        super(codigo,nome,quantidade,limite,preco);
+    constructor(codigo:number, nome:string, quantidade:number, limite:number, preco:number,estoque:gerenciaEstoque){
+        super(codigo,nome,quantidade,limite,preco,estoque);
     } 
 }
 
 class Eletrodomesticos extends Produto{
-    constructor(codigo:number, nome:string, quantidade:number, limite:number, preco:number){
-        super(codigo,nome,quantidade,limite,preco);
-    }   
+    constructor(codigo:number, nome:string, quantidade:number, limite:number, preco:number,estoque:gerenciaEstoque){
+        super(codigo,nome,quantidade,limite,preco,estoque);
+    } 
 }
 
-
-const geladeira = new Eletrodomesticos(231, "Tramontina", 20,5, 100);
-const pc = new Informatica(123, "PC Positivo", 12,10,20.00);
+const estoqueGeladeira = new estoqueLimite(20,5);
+const estoquePc = new estoqueLimite(12,10);
+const geladeira = new Eletrodomesticos(231, "Tramontina",20,5, 100,estoqueGeladeira);
+const pc = new Informatica(123, "PC Positivo", 12,10,20.00,estoquePc);
 
 Produto.listaprodutos();
 console.log(pc.getQuantidade());
